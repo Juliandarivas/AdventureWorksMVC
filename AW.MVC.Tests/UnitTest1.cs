6b5a81317bf53dc915354a72c5b8c2916f788684
juliandarivas <juliandarivas@hotmail.com>
2018-09-06 23:53:14 -0500

    Se añaden los modelos del esquema "Person" y se crea el proyecto de pruebas

---
+++ AW.MVC.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AW.MVC.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
